be8a0f23070a20aacfdb6731553fc4076a154e97
daoodaba975 <davinhoba9@outlook.fr>
2024-12-05 21:00:35 +0000

    [FEAT]: Add new project using jsonplaceholder API ⚡

---
+++ app/userlist/lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'dart:convert'; // Pour décoder le JSON
+import 'package:http/http.dart' as http; // Pour les requêtes HTTP
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<User>> users;
+
+  @override
+  void initState() {
+    super.initState();
+    users = fetchUsers();
+  }
+
+  Future<List<User>> fetchUsers() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+    if (response.statusCode == 200) {
+      // Décoder la réponse JSON
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((user) => User.fromJson(user)).toList();
+    } else {
+      throw Exception('Échec du chargement des utilisateurs');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Liste des Utilisateurs'),
+      ),
+      body: FutureBuilder<List<User>>(
+        future: users,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Erreur : ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final user = snapshot.data![index];
+                return ListTile(
+                  title: Text(user.name),
+                  subtitle: Text(user.email),
+                  trailing: Text(user.username),
+                );
+              },
+            );
+          } else {
+            return Center(child: Text('Aucun utilisateur trouvé.'));
+          }
+        },
+      ),
+    );
+  }
+}
+
+// Classe pour représenter un utilisateur
+class User {
+  final int id;
+  final String name;
+  final String email;
+  final String username;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.username,
+  });
+
+  // Factory pour créer un utilisateur à partir du JSON
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      username: json['username'],
+    );
+  }
+}
